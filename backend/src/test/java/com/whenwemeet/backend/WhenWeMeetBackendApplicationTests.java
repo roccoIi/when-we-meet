01bca69a90f6e956abe0d72a0d6f8eb1dd030919
황정현 <wjdgus982@naver.com>
2026-01-27 14:12:00 +0900

    init: backend initailize

---
+++ backend/src/test/java/com/whenwemeet/backend/WhenWeMeetBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whenwemeet.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhenWeMeetBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
